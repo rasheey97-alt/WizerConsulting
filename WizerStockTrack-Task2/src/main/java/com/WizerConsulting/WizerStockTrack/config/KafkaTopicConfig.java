bdc791f0cd8381620a6c2b3b036155eb1c14a3d9
rasheey97-alt <rasheey97@yahoo.com.com>
2024-12-31 10:35:41 +0100

    this is the last commit

--- WizerStockTrack-Task2/src/main/java/com/WizerConsulting/WizerStockTrack/config/KafkaTopicConfig.java
+++ WizerStockTrack-Task2/src/main/java/com/WizerConsulting/WizerStockTrack/config/KafkaTopicConfig.java
@@ -1,13 +1,26 @@
 package com.WizerConsulting.WizerStockTrack.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+
 @Configuration
 public class KafkaTopicConfig {
-    public NewTopic orderCreationTopic() {
-        return TopicBuilder.name("orderCreation")
+
+    @Value  ("${spring.kafka.topic.orderCreation}")
+    private String topicName;
+
+    @Value("${spring.kafka.topic-json.orderCreation}")
+    private String orderCreation;
+
+    @Bean
+    public NewTopic orderCreationTopic(){
+        return TopicBuilder.name(orderCreation)
                 .build();
-   }
+    }
+
+
 }
